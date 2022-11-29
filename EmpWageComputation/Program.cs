8e5884e08b205ed5d863a3dc0af4c85f880b9fd7
Hemyani <hdg2512@gmail.com>
2022-11-29 18:21:48 +0530

    Solving using Switch Case Statement

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
+
 namespace EmpWageComputation2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            UC3.AddPartTime();
+            UC4.EmpWages();
             Console.ReadLine();
         }
     }
