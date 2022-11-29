31c612096b8920a6b5f6f12213e8ec47b1e25a13
Hemyani <hdg2512@gmail.com>
2022-11-29 18:10:13 +0530

    Check Employee is Present or Absent

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -5,26 +5,8 @@ namespace EmpWageComputation2
     {
         static void Main(string[] args)
         {
-            //UC1.EmpAttendance();
-            //Console.ReadLine();
-
-            //UC2.EmpWage();    
-            //Console.ReadLine();
-
-            //UC3.AddPartTime();
-            //Console.ReadLine();
-
-            //UC4.EmpWages();
-            //Console.ReadLine();
-
-            //UC5.EmpWages();
-            //Console.ReadLine();
-
-            //UC6.DaysOrHours();
-            //Console.ReadLine();
-
-            //UC7.Refactor();
-            //Console.ReadLine();
+            UC1.EmpAttendance();
+            Console.ReadLine();
         }
     }
 }
